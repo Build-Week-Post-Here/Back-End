89ca0422aafbdf8210c03c77c2e3290c2e1df5a8
Kristin Barr <kristinbarr@gmail.com>
2019-10-21 13:25:20 -0400

    auth router and middleware

---
+++ auth/auth-router.js
@@ -0,0 +1,52 @@
+const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const generateToken = require('./token-middleware')
+const Users = require('../posts/users-model')
+
+router.post('/register', (req, res) => {
+  let { username, password } = req.body // deconstruct user sent in request
+
+  // checks if username is already taken
+  Users.findBy({ username }).then((newUser) => {
+    if (newUser) {
+      res.status(401).json({ message: 'username is already taken' })
+    }
+  })
+
+  const hash = bcrypt.hashSync(password, 10) // hash the password
+  password = hash // set hased password as the password
+
+  // add the new user to database
+  Users.add({ username, password })
+    .then((saved) => {
+      res.status(201).json(saved) // send back the saved user
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'cannot add the user', err })
+    })
+})
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body // capture username, pass from request
+
+  Users.findBy({ username })
+    .first()
+    .then((user) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        // generate a token
+        const token = generateToken(user)
+        // add token to response
+        res.status(200).json({
+          message: `Welcome ${user.username}`,
+          token
+        })
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' })
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err)
+    })
+})
+
+module.exports = router
