a258d8112c8622e1e3a1e2da10522c7b1ba8851c
Kristin Barr <kristinbarr@gmail.com>
2019-10-23 01:16:01 -0400

    adds users post, put, delete routes

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -2,11 +2,12 @@
 
 const router = require('express').Router()
 const Posts = require('./posts-model')
+const Users = require('../users/users-model')
 
-// api/posts/:id/user - get a specific users posts
-router.get('/:id/user', (req, res) => {
-  const userId = req.params.id
-  Posts.findAll(userId)
+// GET /api/posts/:userid/posts - get a specific users posts
+router.get('/:userid/user', (req, res) => {
+  const { userid } = req.params
+  Posts.findAllPosts(userid)
     .then((posts) => {
       res.status(200).json(posts)
     })
@@ -14,23 +15,75 @@ router.get('/:id/user', (req, res) => {
       res.status(500).json({ message: 'error finding posts' })
     })
 })
-// api/posts/:id - get a specific post
-router.get('/:id', (req, res) => {
-  const { id } = req.params
-  Posts.findById(id)
+
+// GET /api/posts/:postid - get a user's specific post
+router.get('/:postid', (req, res) => {
+  const { postid } = req.params
+  Posts.findPostById(postid)
     .then((post) => {
-      req.status(200).json(post)
+      res.status(200).json(post)
     })
     .catch((err) => {
       res.status(500).json({ message: 'error finding post' })
     })
 })
 
-// router.post('/', (req, res) => {
-//   const post = req.body
+// POST /api/posts/:userid - save a users new post
+router.post('/:userid', (req, res) => {
+  const post = req.body
+  // setting param id as post's user_id
+  post.user_id = req.params.userid
+  const { userid } = req.params
 
-//   Posts.add(post)
-// })
+  Users.findUserById(userid)
+    .then(user => {
+      // checks if current user exists
+      if (user) {
+        Posts.add(post, userid)
+          .then(post => {
+            res.status(201).json(post)
+          })
+          .catch(err => {
+            res.status(500).json({ message: 'Failed to save new post'})
+          })
+      }
+    })
+
+})
+
+// UPDATE /api/posts/:postid - update a users post
+router.put('/:postid', (req, res) => {
+  const changes = req.body
+  const { postid } = req.params
+  Posts.findPostById(postid)
+    .then((user) => {
+      if (user) { // if user is in db, we can add the post
+        Posts.update(postid, changes)
+          .then(changes => {
+            res.status(200).json(changes)
+          })
+      } else {
+        res.status(500).json({ message: 'Failed to update post' })
+      }
+    })
+})
+
+// DELETE /api/posts/:postid - delete a users post
+router.delete('/:postid', (req, res) => {
+  const { postid } = req.params
+
+  Posts.remove(postid)
+    .then((deleted) => {
+      if (deleted) {
+        res.status(200).json({ removed: deleted })
+      } else {
+        res.status(404).json({ message: 'Could not find post with given id' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to delete post' })
+    })
+})
 
 // const requestOptions = {
 //   headers: { accept: 'application/json' }
