29799360d52f1b5f148f4eb494dcd168a9414ad7
Kristin Barr <kristinbarr@gmail.com>
2019-10-23 23:59:04 -0400

    updated rec and post update

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -31,49 +31,53 @@ router.get('/:postid', (req, res) => {
 router.post('/:userid', (req, res) => {
   const entry = req.body
   const { userid } = req.params
-  // setting param id as post's user_id
+  // sets param id as post's user_id property
   entry.post.user_id = userid
 
+  // checks if current user exists
   Users.findUserById(userid)
     .then(user => {
-      if (user) { // checks if current user exists
-        Posts.add(entry, userid)
+      if (user) {
+        // adds new post
+        Posts.add(entry)
           .then(post => {
-            console.log('ROUTER saved post: ', post)
-            // setting newly created post id as rec's post_id
+            // sets new post id as each rec's post_id property
             entry.rec1.post_id = post.id
             entry.rec2.post_id = post.id
             entry.rec3.post_id = post.id
             entry.rec4.post_id = post.id
             entry.rec5.post_id = post.id
-
+            // adds new post recs
             Recs.saveRecs(entry)
               .then(recs => res.status(201).json(recs))
               .catch(err =>res.status(401).json({message: "error saving recs"}))
           })
           .catch(err => res.status(401).json({ message: "error saving post" }))
       }
-
     })
     .catch(err => res.status(500).json({ message: 'Failed to save new post'}))
-
 })
 
 // UPDATE /api/posts/:postid - update a users post
 router.put('/:postid', (req, res) => {
   const changes = req.body
   const { postid } = req.params
+
+  // checks if post exists
   Posts.findPostById(postid)
-    .then((user) => {
-      if (user) { // if user is in db, we can add the post
-        Posts.update(postid, changes)
+    .then((post) => {
+      if (post) {
+        // updates existing post
+        Posts.update(changes.post, postid)
           .then(changes => {
-            res.status(200).json(changes)
+
+            Recs.updateRecs(changes, postid)
+            .then(recs => res.status(200).json(changes))
+            .catch(err =>res.status(401).json({message: "error updating recs"}))
           })
-      } else {
-        res.status(500).json({ message: 'Failed to update post' })
       }
     })
+    .catch(err => res.status(500).json({ message: 'Failed to update post' }))
 })
 
 // DELETE /api/posts/:postid - delete a users post
