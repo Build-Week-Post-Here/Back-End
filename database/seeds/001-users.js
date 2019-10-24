4b0f5b0d88a43eafea6f8e9601aa711343f5cd34
Kristin Barr <kristinbarr@gmail.com>
2019-10-24 12:15:32 -0400

    seed data test accounts working

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -1,3 +1,11 @@
+const bcrypt = require('bcryptjs')
+
+const user1 = {email: 'joe1337@me.com', password: 'password', profile_img: null}
+user1.password = bcrypt.hashSync(user1.password, 10)
+const user2 = {email:'fakenews@me.com', password: 'passpass', profile_img: null}
+user2.password = bcrypt.hashSync(user2.password, 10)
+const user3 = {email: 'grandma@mail.com', password: 'password', profile_img: null}
+user3.password = bcrypt.hashSync(user3.password, 10)
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
@@ -5,9 +13,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {email: 'joe1337@me.com', password: 'joe000', profile_img: null},
-        {email: 'fakenews99@me.com', password: 'passpass', profile_img: null},
-        {email: 'gma3000@me.com', password: 'grandma', profile_img: null}
+        user1,
+        user2,
+        user3
       ]);
     });
 };
