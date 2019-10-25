28fea2ac1c520c5ae90ffe497b700ae228b60550
Kristin Barr <kristinbarr@gmail.com>
2019-10-25 17:05:38 -0400

    added secret for test functionality

--- auth/token-middleware.js
+++ auth/token-middleware.js
@@ -1,16 +1,17 @@
-
 const jwt = require('jsonwebtoken')
+const secret = require('../config/secrets')
 
 function generateToken(user) {
   const payload = {
     email: user.email,
     id: user.id
   }
+
   const options = {
     expiresIn: '1h'
   }
 
-  return jwt.sign(payload, process.env.JWT_SECRET, options)
+  return jwt.sign(payload, process.env.JWT_SECRET || secret.jwtSecret, options)
 }
 
 module.exports = generateToken
